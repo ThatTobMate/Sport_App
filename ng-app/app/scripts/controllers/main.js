0b8b5f9d00a6c9d91d35dbb98cb2344ec3b82bcc
ThatTobMate <thale.android@gmail.com>
2015-07-13 21:01:35 +0100

    first commit

---
+++ ng-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sportsAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sportsAppApp
+ */
+angular.module('sportsAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
