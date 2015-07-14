d0b95ddd57716f0e92233c192c0b31341444a2c8
ThatTobMate <thale.android@gmail.com>
2015-07-14 22:31:52 +0100

    log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  scope '/api' do
+    devise_for :users
+  end
 
-  get '/', to: redirect('/')
+  # get '/', to: redirect('/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- ng-app/app/scripts/app.js
+++ ng-app/app/scripts/app.js
@@ -19,39 +19,53 @@ angular
     'Devise',
     'ui.router'
   ])
-  .config(['$stateProvider', '$urlRouterProvider',function ($routeProvider, $stateProvider, $urlRouterProvider) {
-
-    $stateProvider.state('about', {
-        url: '/about',
-        templateUrl: 'about.html',
-        controller: 'AboutCtrl'
+  .config(function ($routeProvider, $stateProvider, $urlRouterProvider, AuthProvider) {
+    // $routeProvider
+    //   .when('/', {
+    //     templateUrl: 'views/home.html',
+    //     controller: 'MainCtrl'
+    //   })
+    //   .when('/main', {
+    //     templateUrl: 'views/main.html',
+    //     controller: 'MainCtrl'
+    //   })
+    //   .when('/about', {
+    //     templateUrl: 'views/about.html',
+    //     controller: 'AboutCtrl'
+    //   })
+    $stateProvider.state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
       })
-      // .state('login', {
-      //   url: '/login',
-      //   templateUrl: 'auth/_login.html',
-      //   controller: 'AuthCtrl',
-      //   onEnter: ['$state', 'Auth', function($state, Auth) {
-      //     Auth.currentUser().then(function (){
-      //       $state.go('home');
-      //     })
-      //   }]
-      // })
-      // .state('register', {
-      //   url: '/register',
-      //   templateUrl: 'auth/_register.html',
-      //   controller: 'AuthCtrl',
-      //   onEnter: ['$state', 'Auth', function($state, Auth) {
-      //     Auth.currentUser().then(function (){
-      //       $state.go('home');
-      //     })
-      //   }]
-      // });
+      .state('login', {
+        url: '/login',
+        templateUrl: 'views/auth/_login.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function (){
+            $state.go('home');
+          })
+        }]
+      })
+      .state('register', {
+        url: '/register',
+        templateUrl: 'views/auth/_register.html',
+        controller: 'AuthCtrl',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function (){
+            $state.go('home');
+          })
+        }]
+      });
+    // $urlRouterProvider.otherwise('home')
+      AuthProvider.loginPath('api/users/sign_in.json');
+      AuthProvider.loginMethod('GET');
+      AuthProvider.resourceName('customer');
+      AuthProvider.logoutPath('api/users/sign_out.json');
+      AuthProvider.logoutMethod('GET');
+      AuthProvider.registerPath('api/users.json');
+      AuthProvider.registerMethod('GET');
+      AuthProvider.resourceName('customer');
 
-      // $routeProvider
-      //   .when('/about', {
-      //     templateUrl: 'views/about.html',
-      //     controller: 'AboutCtrl'
-      //   })
-
-    $urlRouterProvider.otherwise('about');
-  }])
+  })
